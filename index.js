c2c835d6c38217391bcbf63b02156c01e117c5e1
rajat9024 <8824rajat@gmail.com>
2024-06-13 13:59:27 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,162 @@
+// var num = 10;
+
+// {
+//     num=20;
+// }
+// console.log("var",num)
+
+// //let
+// let num1 =10
+// {
+//     num1=20
+// }
+// console.log("let",num1)
+
+// //const
+// const num2=10
+// {
+//     num2=20
+// }
+// console.log("const",num2)
+
+
+// var num = 10;
+
+// {
+//   var num=20;
+//   num=30;
+//   console.log(num)
+// }
+// console.log("var",num)
+
+// //let
+// let num1 =10
+// {
+//    let num1=20
+//    console.log(num1)
+// }
+// console.log("let",num1)
+
+// //const
+// const num2=10
+// {
+//    const num2=20
+//    console.log(num2)
+// }
+// console.log("const",num2)
+
+// let num1=10;
+// let num2=20;
+// let sum=num1 + num2;
+// console.log("sum=",sum) 
+// let sub=num1-num2;
+// console.log("sub=",sub)
+// let mul=num1*num2;
+// console.log("mul=",mul)
+// let div=num1/num2;
+// console.log("div=",div)
+// let mod=num1%num2;
+// console.log("mod=",mod)
+
+// let num=5
+// let num1=10
+// let sum=--num + ++num1 + num-- + num1++
+// console.log(sum)
+// console.log(num)
+// console.log(num1)
+
+// let a=10
+// let b=20
+// let c=30
+// let result=(a>b && a>c)?"a":(b>c)?"b":"c";
+// console.log(result)
+
+// let age=20
+// let result=(age>18)?"eligible":"not eligible";
+// console.log(result)
+
+// function add()
+// {
+//     console.log("Hello")
+// }
+// add();
+
+// function add(...a)
+// {
+//     console.log(a)
+// }
+// add(10,"RAjat",50)
+// function add(b,...a)
+// {
+//    console.log(b)
+// }
+// add(10,"RAjat",50)
+
+// (()=>
+// {
+//     console.log("hello")
+// })()
+    
+// let name ='it"s Hello'
+// console.log(name);
+
+// let ischeck =true
+// console.log(ischeck);
+
+//  function add(num){
+//     // console.log("my pocket money is "+num);
+//     console.log(`my pocket money is ${num+num} in current time`);
+
+//  }               
+//  add(100)
+
+//  let str="monday"
+//  switch (str) {
+//     case "monday":
+//         console.log("today monday");
+//         break;
+//     case "tue":
+//         console.log("today tue");
+//         break;
+//     case "wed":
+//         console.log("today wed");
+//         break;
+//       default:
+//         console.log("not match");     
+//      }
+   
+// let i=0;
+// while(i<5){
+//     console.log("hello");
+//     i++;
+// }
+// let i=5;
+// while(i>0){
+//     console.log("hello");
+//     i--;
+// }
+// let i=1
+// while(i<=10)
+// {
+//     console.log(2*i);
+//     i++
+// }
+// let i=3
+// while(i<=30)
+// {
+//     console.log(i);
+//     i=i+3
+// }
+
+// let i=1
+// do{
+//     console.log(i*2);
+//     i++;
+// }while(i<11)
+
+// let
+let date =new Date()
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getDate());
